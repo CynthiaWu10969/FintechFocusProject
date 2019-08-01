0186624b3303018e4240bf8d4db5caa79f436428
EC2 Default User <ec2-user@ip-172-31-40-72.us-east-2.compute.internal>
2019-08-01 20:09:57 +0000

    all tied and with a template

---
+++ app/models/model.py
@@ -0,0 +1,35 @@
+import requests
+
+def shares_calculator(symbol, initial_balance, initial_day):
+    result = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=' + symbol + '&apikey=6NRP03K3NSL2U5UK')
+    dict = result.json()
+    result_dict = dict["Weekly Adjusted Time Series"]
+    initial_price = 0
+    for key in result_dict.keys():
+        if (initial_day == key):
+            initial_price = float(result_dict[initial_day]["5. adjusted close"])
+    print(result_dict)
+    print (initial_balance)
+    print(initial_price)
+    shares_all = initial_balance // initial_price
+    return shares_all
+    
+def balance_calculator(shares_all, shares, symbol, initial_balance, initial_day, sell_day):
+    result = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=' + symbol + '&apikey=6NRP03K3NSL2U5UK')
+    dict = result.json()
+    result_dict = dict["Weekly Adjusted Time Series"]
+    for key in result_dict.keys():
+        if (initial_day == key):
+            initial_price = float(result_dict[initial_day]["5. adjusted close"])
+    if shares < shares_all:
+        for key in result_dict.keys():
+            if (sell_day == key):
+                sell_price = float(result_dict[sell_day]["5. adjusted close"])
+        profit = (sell_price - initial_price) * shares
+        balance = initial_balance + profit
+        return balance
+    else:
+        return "Please enter a valid shares"
+        
+# print(shares_calculator('AAPL', 2900, "1990"))
+# print(balance_calculator(29, 25, 'AAPL', 2900, "1990", "2019"))
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,62 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app.models import model, formopener
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+    
+@app.route('/stockmarket')
+def stockmarket():
+    return render_template('stockmarket.html')
+    
+@app.route('/instructions')
+def instructions():
+    return render_template('instructions.html')
+
+@app.route('/aboutgame')
+def aboutgame():
+    return render_template('aboutgame.html')
+    
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/cong')
+def cong():
+    return render_template('cong.html')
+    
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+    
+@app.route('/shares', methods = ['GET', 'POST'])
+def shares():
+    if request.method == "GET":
+        return "Please fill out the form."
+    else:
+        userdata = dict(request.form)
+        print(userdata)
+        all_shares = model.shares_calculator(userdata['symbol'], float(userdata['initial_balance']), userdata['day'])
+        return render_template('shares.html', all_shares = all_shares, initial_day = userdata['day'], initial_balance = float(userdata['initial_balance']), symbol = userdata['symbol'])
+        
+
+@app.route('/finalbalance', methods = ['GET', 'POST'])
+def finalbalance():
+    if request.method == "GET":
+        return "Please fill out the form."
+    else:
+        userdata = dict(request.form)
+        total_sharesfinal = float(userdata['all_shares'])
+        final_balance = round(model.balance_calculator(total_sharesfinal, float(userdata['shares']), userdata['symbol'], float(userdata['initial_balance']), userdata['initial_day'], userdata['sell_day']), 2)
+        return render_template('finalbalance.html', finalbalance = final_balance, symbol = userdata['symbol'])
+        
+@app.route('/weatherfinal', methods = ['GET', 'POST'])
+def weatherfinal():
+    if request.method == "GET":
+        return "Please fill out the form"
+    else:
+        userdata = dict(request.form)
+        print(userdata)
+        return "hi"
